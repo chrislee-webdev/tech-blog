2de0aa6a0400bc9e3c15c7c069cd5b2adb230795
Christopher <leechrs1@gmail.com>
2022-07-13 23:16:17 -0500

    still can't login

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -17,17 +20,14 @@ const sess = {
   })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
