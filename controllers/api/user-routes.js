08772077b1b3a15fa4e5cdff964a3a228d14b5bb
Christopher <leechrs1@gmail.com>
2022-07-13 22:56:23 -0500

    can't log in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -79,10 +79,16 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
+
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.name;
+        req.session.loggedIn = true;
   
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
+});
 
 // update user
 router.put('/:id', (req, res) => {
@@ -93,16 +99,18 @@ router.put('/:id', (req, res) => {
         }
     })
     .then(dbUserData => {
-        if (!dbUserData[0]) {
-            res.status(404).json({ message: 'No user found' });
-            return;
-        }
-        res.json(dbUserData);
-    })
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = truel
+
+            res.json(dbUserData);
+        })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
 });
 
 // delete user
