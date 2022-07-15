3d6611a4b80b5828a4cccd0150502cdac7779155
Christopher <leechrs1@gmail.com>
2022-07-14 20:16:04 -0500

    feature/login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,8 +55,14 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+    .then(dbUserData => {
+        req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json({user:dbUserData,message:"User created"})
+        })
+    }).catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
@@ -89,6 +95,16 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
 
 // update user
 router.put('/:id', (req, res) => {
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    const userid =  req.session.user_id
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: userid
+      },
+      include: [
+        {
+          model: Post,
+          
+        },
+        {
+          model: Comment,
+         
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        }
+      ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        // res.json(dbUserData);
+        res.render("dashboard",dbUserData)
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
