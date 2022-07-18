defb559854bbd89466a0476cc270bbee76f883c9
Christopher <leechrs1@gmail.com>
2022-07-17 21:47:35 -0500

    still can't get the dashboard to render properly

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
           return;
         }
         // res.json(dbUserData);
-        res.render("dashboard",dbUserData)
+        res.render("dashboard", dbUserData)
       })
       .catch(err => {
         console.log(err);
